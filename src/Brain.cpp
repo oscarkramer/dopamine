479ff812e1132310b8c90efc18e494d4b2697d28
oscarkramer <oscar.kramer@radiantsolutions.com>
2018-09-26 23:46:03 -0400

    Design

--- src/Brain.cpp
+++ src/Brain.cpp
@@ -5,7 +5,6 @@ using namespace std;
 Brain::Brain()
 {
    // Initialize network:
-
    // Load prior memory states:
 
 }
@@ -23,20 +22,20 @@ void Brain::stimulate(const vector<double> perception)
 
 const vector<double> Brain::decision()
 {
-
+   // Fetch the current state of the cognitive system for external evaluation:
 }
 
 void Brain::reward(const double& level)
 {
-
+   // Reinforce(+) or discourage(-) the prior decision
 }
 
 void Brain::sleep()
 {
-
+   // Change mode to permit rewiring given memories and expectations
 }
 
 void Brain::age()
 {
-
+   // Attenuate synapse weights for little-used pathways
 }
